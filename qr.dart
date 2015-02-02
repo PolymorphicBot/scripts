8749936957069bedb379a156fed4ee816bae9b9a
Kenneth Endfinger <kaendfinger@gmail.com>
2015-02-02 09:12:19 -0500

    Use a different QR Generator API

--- qr.dart
+++ qr.dart
@@ -8,5 +8,5 @@ BotConnector bot;
 
 @Command("qr", description: "Generate QR Codes", usage: "<input>")
 qr(CommandEvent event) => event.transform((input) {
-  return shortenUrl("https://chart.googleapis.com/chart?chs=178x178&cht=qr&chl=${Uri.encodeComponent(input)}");
+  return shortenUrl("https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${Uri.encodeComponent(input)}");
 }, prefix: "QR");
