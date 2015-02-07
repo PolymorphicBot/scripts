ac9b629d5f86f0378cf00658728503a8832722d2
Kenneth Endfinger <kaendfinger@gmail.com>
2015-02-07 14:11:54 -0600

    systemctl commands

--- systemctl.dart
+++ systemctl.dart
@@ -64,6 +64,32 @@ systemctl(CommandEvent event) {
       
       event.reply("Restarted '${args[0]}'.", prefixContent: "Services");
     });
+  } else if (cmd == "is-active") {
+    if (args.length != 1) {
+      event.usage();
+      return;
+    }
+  
+    ProcessHelper.run("sudo", ["systemctl", "is-active", args[0]]).then((result) {
+      var exitCode = result.exitCode;
+      
+      if (exitCode != 0) {
+        event.reply("'${args[0]}' is not running.", prefixContent: "Services");
+        return;
+      }
+      
+      event.reply("'${args[0]}' is running.", prefixContent: "Services");
+    });
+  } else if (cmd == "status") {
+    if (args.length != 1) {
+      event.usage();
+      return;
+    }
+  
+    ProcessHelper.getStdout("sudo", ["systemctl", "is-active", args[0]]).then((status) {
+      status = status.trim();
+      event.reply("Status: ${status}", prefixContent: "Services");
+    });
   } else {
     event.reply("Unknown Command", prefixContent: "Services");
   }
