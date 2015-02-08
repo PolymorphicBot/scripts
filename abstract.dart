2e2f2d4bfa9244b3c3eb6c04361d600765d84822
Kenneth Endfinger <kaendfinger@gmail.com>
2015-02-07 19:44:29 -0500

    :D

---
+++ abstract.dart
@@ -0,0 +1,35 @@
+import "package:polymorphic_bot/plugin.dart";
+export "package:polymorphic_bot/plugin.dart";
+
+@PluginInstance()
+Plugin plugin;
+@BotInstance()
+BotConnector bot;
+
+@Command("abstract", description: "Provides an abstraction of a topic.")
+abstract(CommandEvent event) {
+  event.transform((input) {
+    var url = "http://api.duckduckgo.com/?format=json&q=${Uri.encodeComponent(input)}";
+    event.fetchJSON(url).then((json) {
+      var topic = json.RelatedTopics != null ? json.RelatedTopics[0] : null;
+
+      if (json.AbstractText != null) {
+        event.reply(json.AbstractText);
+
+        if (json.AbstractURL != null) {
+          event.reply(json.AbstractURL);
+        }
+      } else if (topic != null && !(new RegExp("\/c\/").hasMatch(topic.FirstURL))) {
+        event.reply(topic.text);
+        event.reply(topic.FirstURL);
+      } else if (json.Definition != null) {
+        event.reply(json.Definition);
+        if (json.DefinitionURL != null) {
+          event.reply(json.DefinitionURL);
+        }
+      } else {
+        event.reply("I don't know anything about that.");
+      }
+    });
+  });
+}
--- hammer.dart
+++ hammer.dart
@@ -2,10 +2,10 @@ import "package:polymorphic_bot/plugin.dart";
 export "package:polymorphic_bot/plugin.dart";
 
 @Command("hammer")
-hammer(CommandEvent event) => event.reply(repeat("\u25AC", 4) + "\u258B");
+hammer(CommandEvent event) => event.reply(("\u25AC" * 4) + "\u258B");
 
 @Command("banhammer")
-banhammer(CommandEvent event) => event.reply("Somebody is bringing out the ban hammer! ${repeat("\u25AC", 4)}\u258B Ò╭╮Ó");
+banhammer(CommandEvent event) => event.reply("Somebody is bringing out the ban hammer! ${"\u25AC" * 4}\u258B Ò╭╮Ó");
 
 @Command("hammertime")
 hammertime(CommandEvent event) {
