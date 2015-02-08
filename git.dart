9e3d16b72f6d6b314d1149e4b5a2e106dc17a66f
Kenneth Endfinger <kaendfinger@gmail.com>
2015-02-08 14:18:12 -0500

    Remove unneeded stuff.

--- git.dart
+++ git.dart
@@ -1,11 +1,6 @@
 import "package:polymorphic_bot/plugin.dart";
 export "package:polymorphic_bot/plugin.dart";
 
-@PluginInstance()
-Plugin plugin;
-@BotInstance()
-BotConnector bot;
-
 @Command("git-version", description: "Git Version")
 gitVersion(CommandEvent event) async {
   var data = await event.fetch("https://raw.githubusercontent.com/git/git/master/RelNotes");
