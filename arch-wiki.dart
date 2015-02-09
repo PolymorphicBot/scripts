ab2e8c4a8a5dee021e02ed41aecf99c13b42b7ba
Kenneth Endfinger <kaendfinger@gmail.com>
2015-02-09 16:46:46 -0500

    Update arch-wiki.dart

--- arch-wiki.dart
+++ arch-wiki.dart
@@ -2,7 +2,7 @@ import "package:polymorphic_bot/plugin.dart";
 export "package:polymorphic_bot/plugin.dart";
  
 @Command("arch-wiki", description: "Search the Arch Linux Wiki")
-archWiki(event) => event << (input) {
+archWiki(event) => event >> (input) {
   return event.fetchJSON("https://wiki.archlinux.org/api.php?action=query&prop=info&inprop=url&format=json&titles=${Uri.encodeComponent(input)}").then((json) {
     if (json.pages.isEmpty || json.pages.containsKey("-1")) {
       return "> No Wiki Page Found.";
