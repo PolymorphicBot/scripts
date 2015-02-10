50ba2793d4a5b536a4f7559dc0bfd3e7f0f127cd
Kenneth Endfinger <kaendfinger@gmail.com>
2015-02-10 13:47:37 -0500

    Add Method Definition on explainshell.

--- explainshell.dart
+++ explainshell.dart
@@ -4,7 +4,7 @@ export "package:polymorphic_bot/plugin.dart";
 @Command("explainshell", description: "Explains Shell Commands", prefix: "Explain Shell")
 explainShell(input) => "http://explainshell.com/explain?cmd=${Uri.encodeComponent(input).replaceAll("%20", "+")}";
 
-@NotifyPlugin("link_title")
+@NotifyPlugin("link_title", methods: const ["blacklistMessage"])
 blacklistLinkTitle(linkTitle) {
   linkTitle.blacklistMessage("//explainshell.com/");
 }
