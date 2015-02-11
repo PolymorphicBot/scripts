e0d71afb9001a37e67242757a08648374b8598e3
Kenneth Endfinger <kaendfinger@gmail.com>
2015-02-10 23:28:30 -0500

    Imgur Plugin

---
+++ imgur.dart
@@ -0,0 +1,39 @@
+import "package:polymorphic_bot/plugin.dart";
+export "package:polymorphic_bot/plugin.dart";
+
+const List<String> TOPICS = const [
+  "unicorn",
+  "funny",
+  "dog",
+  "cat",
+  "fail"
+];
+
+@Command("imgur", description: "Search Imgur", prefix: "Imgur")
+imgur(CommandEvent event) {
+  var search = event.chooseAtRandom(TOPICS);
+  
+  if (event.hasArguments) {
+    search = event.joinArguments();
+  }
+  
+  return event.fetchJSON("https://api.imgur.com/3/gallery/search", headers: {
+    "Authorization": "Client-ID 540678dd539a986"
+  }, query: {
+    "q": search
+  }).then((json) {
+    var images = json.data;
+    
+    if (images.isNotEmpty) {
+      var image = event.chooseAtRandom(images);
+      return image.link;
+    } else {
+      return "I've got nothing.";
+    }
+  }).catchError((e) {
+    if (e is HttpError) {
+      print(e.statusCode);
+      print(e.response);
+    }
+  });
+}
