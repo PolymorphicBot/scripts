6800b7600b681e14136868fb70c15dfc5619addc
Kenneth Endfinger <kaendfinger@gmail.com>
2015-02-10 22:05:59 -0500

    Lizard Command

--- abstract.dart
+++ abstract.dart
@@ -7,28 +7,26 @@ Plugin plugin;
 BotConnector bot;
 
 @Command("abstract", description: "Provides an abstraction of a topic.")
-abstract(CommandEvent event) {
-  event >> (input) async {
-    var url = "http://api.duckduckgo.com/?format=json&q=${Uri.encodeComponent(input)}";
-    var json = await event.fetchJSON(url);
-    var topic = json.RelatedTopics != null ? json.RelatedTopics[0] : null;
+abstract(CommandEvent event, input) async {
+  var url = "http://api.duckduckgo.com/?format=json&q=${Uri.encodeComponent(input)}";
+  var json = await event.fetchJSON(url);
+  var topic = json.RelatedTopics != null ? json.RelatedTopics[0] : null;
 
-    if (json.AbstractText != null) {
-      event.reply(json.AbstractText);
+  if (json.AbstractText != null) {
+    event << json.AbstractText;
 
-      if (json.AbstractURL != null) {
-        event.reply(json.AbstractURL);
-      }
-    } else if (topic != null && !(new RegExp("\/c\/").hasMatch(topic.FirstURL))) {
-      event.reply(topic.text);
-      event.reply(topic.FirstURL);
-    } else if (json.Definition != null) {
-      event.reply(json.Definition);
-      if (json.DefinitionURL != null) {
-        event.reply(json.DefinitionURL);
-      }
-    } else {
-      event.reply("I don't know anything about that.");
+    if (json.AbstractURL != null) {
+      event << json.AbstractURL;
     }
-  };
+  } else if (topic != null && !(new RegExp("\/c\/").hasMatch(topic.FirstURL))) {
+    event << topic.text;
+    event << topic.FirstURL;
+  } else if (json.Definition != null) {
+    event << json.Definition;
+    if (json.DefinitionURL != null) {
+      event << json.DefinitionURL;
+    }
+  } else {
+    return "I don't know anything about that.";
+  }
 }
