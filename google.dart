1cd92ebb9e0c758b06f0e191fff34629c560a65a
Kenneth Endfinger <kaendfinger@gmail.com>
2015-02-11 11:29:09 -0500

    Bug Fix

--- google.dart
+++ google.dart
@@ -2,7 +2,7 @@ import "package:polymorphic_bot/plugin.dart";
 export "package:polymorphic_bot/plugin.dart";
 
 @Command("google", description: "Google Search", usage: "<query>", prefix: "Google")
-google(input) {
+google(input) async {
   var json = await fetchJSON("http://ajax.googleapis.com/ajax/services/search/web?v=1.0&q=${Uri.encodeComponent(query)}");
   var results = json.responseData.results;
 
