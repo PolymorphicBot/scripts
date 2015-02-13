ba7bd80708b2bcf53c721ff638cee88daab782ee
Kenneth Endfinger <kaendfinger@gmail.com>
2015-02-12 22:29:47 -0500

    Pig Latin

---
+++ pig_latin.dart
@@ -0,0 +1,21 @@
+import "package:polymorphic_bot/plugin.dart";
+export "package:polymorphic_bot/plugin.dart";
+
+@Command("pig-latin", allowVariables: true, description: "Pig Latin Generator")
+pigLatin(input) {
+  var words = input.split(" ");
+  return words.map((it) => it.trim()).map((word) {
+    word = word.toLowerCase();
+    if (!VOWELS.any((it) => word.contains(it))) {
+      return word;
+    }
+    var consonant = firstConsonant(word);
+    return word.replaceFirst(consonant, "") + consonant + "ay";
+  }).toList().join(" ");
+}
+
+const List<String> VOWELS = const ["a", "e", "i", "o", "u"];
+
+String firstConsonant(String word) => new List.generate(word.length, (i) {
+  return word[i];
+}, growable: true).firstWhere((String it) => !VOWELS.contains(it.toLowerCase()), orElse: () => null);
