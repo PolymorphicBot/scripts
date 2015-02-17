be1e52949477aa8cf85257d461b0dc5aba3c464e
Kenneth Endfinger <kaendfinger@gmail.com>
2015-02-16 23:21:30 -0500

    Kick Rejoin now handles spam reasons.

--- rejoin.dart
+++ rejoin.dart
@@ -1,9 +1,20 @@
 import "package:polymorphic_bot/plugin.dart";
 export "package:polymorphic_bot/plugin.dart";
 
+Scheduler scheduler = new Scheduler();
+const Duration joinDelay = const Duration(seconds: 10);
+
 @OnKick()
 handleKick(KickEvent event) async {
-  if (event.user == await event.bot.getBotNickname(event.network)) {
+  void rejoin() {
     event.bot.joinChannel(event.network, event.channel);
+  }
+  
+  if (event.user == await event.bot.getBotNickname(event.network)) {
+    if (event.reason.toLowerCase().contains("spam")) {
+      scheduler.schedule(joinDelay, rejoin);
+    } else {
+      rejoin();
+    }
   }
 }
