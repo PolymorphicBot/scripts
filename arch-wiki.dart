d366ced1358d5bdf32efee56f079561152f0ddba
Kenneth Endfinger <kaendfinger@gmail.com>
2015-04-08 17:04:38 -0400

    Arch Linux Wiki Tweaks

--- arch-wiki.dart
+++ arch-wiki.dart
@@ -1,11 +1,16 @@
 import "package:polymorphic_bot/plugin.dart";
 export "package:polymorphic_bot/plugin.dart";
- 
+
+const String URL = "https://wiki.archlinux.org/api.php?action=query&prop=info&inprop=url&format=json&titles=";
+
 @Command("arch-wiki", description: "Search the Arch Linux Wiki", prefix: "Arch Wiki")
-archWiki(input) => fetchJSON("https://wiki.archlinux.org/api.php?action=query&prop=info&inprop=url&format=json&titles=${Uri.encodeComponent(input)}").then((json) {
+archWiki(input) async {
+  var url = "${URL}${Uri.encodeComponent(input)}";
+  var json = await fetchJSON(url);
+
   if (json.query.pages.isEmpty || json.query.pages.containsKey("-1")) {
     return "No Wiki Page Found.";
   } else {
     return "${json.query.pages[json.query.pages.keys.first]['fullurl']}";
   }
-});
+}
