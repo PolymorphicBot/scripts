a4e5d06940312631fc2678ff5a406b3e0aada6e0
Kenneth Endfinger <kaendfinger@gmail.com>
2015-04-18 22:42:54 -0400

    Minor Tweaks and Fix RegExp

--- adult.dart
+++ adult.dart
@@ -1,8 +1,8 @@
 import "package:polymorphic_bot/plugin.dart";
 export "package:polymorphic_bot/plugin.dart";
 
-@OnMessage(pattern: r"like an adult\b", regex: true)
-likeAnAdult(event) => event.random([
+@OnMessage(pattern: r"like an adult\b", regex: true, randomize: true)
+likeAnAdult(event) => [
   "http://1.bp.blogspot.com/_D_Z-D2tzi14/TBpOnhVqyAI/AAAAAAAADFU/8tfM4E_Z4pU/s400/responsibility12(alternate).png",
   "http://2.bp.blogspot.com/_D_Z-D2tzi14/TBpOglLvLgI/AAAAAAAADFM/I7_IUXh6v1I/s400/responsibility10.png",
   "http://4.bp.blogspot.com/_D_Z-D2tzi14/TBpOY-GY8TI/AAAAAAAADFE/eboe6ItMldg/s400/responsibility11.png",
@@ -11,4 +11,4 @@ likeAnAdult(event) => event.random([
   "http://3.bp.blogspot.com/_D_Z-D2tzi14/TBpoOlpMa_I/AAAAAAAADGU/CfZVMM9MqsU/s400/responsibility102.png",
   "http://4.bp.blogspot.com/_D_Z-D2tzi14/TBpoVLLDgCI/AAAAAAAADGc/iqux8px_V-s/s400/responsibility12(alternate)2.png",
   "http://2.bp.blogspot.com/_D_Z-D2tzi14/TBpqGvZ7jVI/AAAAAAAADGk/hDTNttRLLks/s400/responsibility8.png"
-]);
+];
--- regex.dart
+++ regex.dart
@@ -8,14 +8,14 @@ Plugin plugin;
 @BotInstance()
 BotConnector bot;
 
-RegExp SEARCH_REPLACE = new RegExp(r"^s\/(.+?)\/(.*?)(?:\/?)([a-z]+?$|$)");
+RegExp SEARCH_REPLACE = new RegExp(r"^s\/(.+?)\/([^\/]+)(?:\/?)([a-z]+?$|$)");
 
 @OnMessage()
 handleMessage(MessageEvent event) {
   if (!SEARCH_REPLACE.hasMatch(event.message)) {
     return;
   }
-  
+
   var match = SEARCH_REPLACE.firstMatch(event.message);
   var search = match.group(1);
   var replace = match.group(2).isEmpty && match.group(3).isNotEmpty ? match.group(3) : match.group(2);
@@ -25,14 +25,14 @@ handleMessage(MessageEvent event) {
   var capitalize = false;
   var caseSensitive = true;
   List<String> mods;
-  
+
   // Fix Stuff
   if (match.group(2).isNotEmpty && match.group(3) != null && match.group(3).isNotEmpty) {
     mods = new List<String>.generate(match.group(3).length, (i) => match.group(3)[i]);
   } else {
     mods = [];
   }
-  
+
   loop: for (var mod in mods) {
     select: switch (mod) {
       case "n":
@@ -55,30 +55,30 @@ handleMessage(MessageEvent event) {
         return;
     }
   }
-  
+
   if (escaped) {
     search = escapeRegex(search);
   }
-  
+
   if (reverse) replace = new String.fromCharCodes(replace.codeUnits.reversed);
-  
+
   RegExp regex;
-  
+
   try {
     regex = new RegExp(search, caseSensitive: caseSensitive);
   } on FormatException catch (e) {
     event.reply("Invalid Expression: ${e.message}", prefixContent: "RegExp");
     return;
   }
-  
+
   bot.getChannelBuffer(event.network, event.channel).then((buffer) {
     for (var entry in buffer) {
       if (entry.message.startsWith("s/") || !(regex.hasMatch(entry.message) || entry.message == search)) {
         continue;
       }
-      
+
       var msg = entry.message;
-      
+
       if (!global) {
         var m = regex.firstMatch(msg);
         var c = m.groupCount;
@@ -86,24 +86,24 @@ handleMessage(MessageEvent event) {
           replace = replace.replaceAll("{${i}}", m.group(i));
         }
       }
-      
+
       var newmsg = global ? msg.replaceAll(regex, replace) : msg.replaceFirst(regex, replace);
-      
+
       if (newmsg.length > 400) {
         newmsg = newmsg.substring(0, 400) + "...";
       }
-      
+
       if (capitalize) {
         newmsg = newmsg[0].toUpperCase() + newmsg.substring(1);
       }
-      
+
       var e = new BufferEntry(entry.network, entry.target, entry.user, newmsg);
-      
+
       event.reply("${entry.user}: ${newmsg}", prefixContent: "RegExp");
       bot.appendChannelBuffer(e);
       return;
     }
-    
+
     event.reply("Failed to find a match. (Modifiers: ${mods})", prefixContent: "RegExp");
   });
 }
